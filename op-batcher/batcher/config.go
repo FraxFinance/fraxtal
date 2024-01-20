3fcffdb117f3597dc22fbcf33bafcdf59271d7b4
Alessandro Tagliapietra <0xalex88@proton.me>
2024-01-20 10:04:35 -0800

    Implement frax DA changes

--- op-batcher/batcher/config.go
+++ op-batcher/batcher/config.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/urfave/cli/v2"
 
+	fraxda "github.com/ethereum-optimism/optimism/frax-da"
 	altda "github.com/ethereum-optimism/optimism/op-alt-da"
 	"github.com/ethereum-optimism/optimism/op-batcher/compressor"
 	"github.com/ethereum-optimism/optimism/op-batcher/flags"
@@ -123,6 +124,7 @@ type CLIConfig struct {
 	PprofConfig   oppprof.CLIConfig
 	RPC           oprpc.CLIConfig
 	AltDA         altda.CLIConfig
+	DaConfig      fraxda.CLIConfig
 }
 
 func (c *CLIConfig) Check() error {
@@ -180,6 +182,9 @@ func (c *CLIConfig) Check() error {
 	if err := c.RPC.Check(); err != nil {
 		return err
 	}
+	if err := c.DaConfig.Check(); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -213,6 +218,7 @@ func NewConfig(ctx *cli.Context) *CLIConfig {
 		MetricsConfig:                opmetrics.ReadCLIConfig(ctx),
 		PprofConfig:                  oppprof.ReadCLIConfig(ctx),
 		RPC:                          oprpc.ReadCLIConfig(ctx),
+		DaConfig:                     fraxda.ReadCLIConfig(ctx),
 		AltDA:                        altda.ReadCLIConfig(ctx),
 		ThrottleThreshold:            ctx.Uint64(flags.ThrottleThresholdFlag.Name),
 		ThrottleTxSize:               ctx.Uint64(flags.ThrottleTxSizeFlag.Name),
