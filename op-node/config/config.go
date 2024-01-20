fa1261bdf47833bff769227c91450d1753624a83
Alessandro Tagliapietra <0xalex88@proton.me>
2024-01-20 10:04:35 -0800

    Implement frax DA changes

--- op-node/config/config.go
+++ op-node/config/config.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/log"
 
+	fraxda "github.com/ethereum-optimism/optimism/frax-da"
 	altda "github.com/ethereum-optimism/optimism/op-alt-da"
 	"github.com/ethereum-optimism/optimism/op-node/flags"
 	"github.com/ethereum-optimism/optimism/op-node/node/tracer"
@@ -86,7 +87,8 @@ type Config struct {
 	ConductorRpcTimeout time.Duration
 
 	// AltDA config
-	AltDA altda.CLIConfig
+	AltDA    altda.CLIConfig
+	DaConfig fraxda.Config
 
 	IgnoreMissingPectraBlobSchedule bool
 	FetchWithdrawalRootFromState    bool
@@ -189,6 +191,10 @@ func (cfg *Config) Check() error {
 	if cfg.AltDA.Enabled {
 		log.Warn("Alt-DA Mode is a Beta feature of the MIT licensed OP Stack.  While it has received initial review from core contributors, it is still undergoing testing, and may have bugs or other issues.")
 	}
+	if err := cfg.DaConfig.Check(); err != nil {
+		return fmt.Errorf("da config error: %w", err)
+	}
+
 	return nil
 }
 
