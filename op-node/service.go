00cca331d4fb4358b37d40eb704c1840ec11be0f
Alessandro Tagliapietra <0xalex88@proton.me>
2024-01-20 10:04:35 -0800

    Implement frax DA changes

--- op-node/service.go
+++ op-node/service.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 
+	fraxda "github.com/ethereum-optimism/optimism/frax-da"
 	altda "github.com/ethereum-optimism/optimism/op-alt-da"
 	"github.com/ethereum-optimism/optimism/op-node/chaincfg"
 	"github.com/ethereum-optimism/optimism/op-node/config"
@@ -125,7 +126,8 @@ func NewConfig(ctx cliiface.Context, log log.Logger) (*config.Config, error) {
 		},
 		ConductorRpcTimeout: ctx.Duration(flags.ConductorRpcTimeoutFlag.Name),
 
-		AltDA: altda.ReadCLIConfig(ctx),
+		AltDA:    altda.ReadCLIConfig(ctx),
+		DaConfig: fraxda.Config(fraxda.ReadCLIConfig(ctx)),
 
 		IgnoreMissingPectraBlobSchedule: ctx.Bool(flags.IgnoreMissingPectraBlobSchedule.Name),
 		FetchWithdrawalRootFromState:    ctx.Bool(flags.FetchWithdrawalRootFromState.Name),
