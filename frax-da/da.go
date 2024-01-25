1ff3d597f54bd840377f1d86fad1865f4bfa0ad5
Alessandro Tagliapietra <0xalex88@proton.me>
2024-01-25 15:19:07 -0800

    Support for older celestia data

--- frax-da/da.go
+++ frax-da/da.go
@@ -2,3 +2,6 @@ package fraxda
 
 // DerivationVersionFraxDa is a byte prefix of frax DA based references
 const DerivationVersionFraxDa = 0xfc
+
+// DerivationVersionCelestia is used for retrocompatibility with the old testnet data
+const DerivationVersionCelestia = 0xce
